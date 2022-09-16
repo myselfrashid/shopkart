a9ee2d780482a85ed6dfa0a8d57e510922500935
myselfrashid <rashidali.jmi@gmail.com>
2022-09-16 13:09:09 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import {Routes, Route} from 'react-router-dom';
+import ProductInfo from './components/ProductInfo';
+import { Product } from './components/Product';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route exact path = "/" element = {<Home />} />
+      <Route exact path = "/products" element = {<Product />} />
+      <Route exact path = "/products/:id" element = {<ProductInfo />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import manageCart from "./manageCart";
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    manageCart,
+})
+
+export default rootReducer;
